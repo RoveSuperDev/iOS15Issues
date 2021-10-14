7fdc00115f19919dd3b0a01703d5583736eee1d7
RoveSuperDev <75606458+RoveSuperDev@users.noreply.github.com>
2021-10-14 19:55:29 +0800

    Update code.m

--- code.m
+++ code.m
@@ -2,10 +2,16 @@
 
 
 
- if (@available(iOS 15.0, *))
+   self.tabBar.layer.shadowColor = RSD_Color(197, 198, 209).CGColor;
+    self.tabBar.layer.shadowOffset = CGSizeMake(0, -2);
+    self.tabBar.layer.shadowOpacity = 0.25;
+    if (@available(iOS 15.0, *))
     {
         UITabBarAppearance *appearance = [[UITabBarAppearance alloc]init];
+        [appearance configureWithOpaqueBackground];
         appearance.backgroundColor = RSD_Color(240, 240, 240);
+        appearance.shadowImage = [UIImage rsd_imageWithColor:RSD_Color(240, 240, 240)size:CGSizeMake(1, 0.5)];
+        appearance.backgroundImage = [UIImage rsd_imageWithColor:RSD_Color(255, 255, 255)size:CGSizeMake(1, 0.5)];
         appearance.stackedLayoutAppearance.selected.titleTextAttributes = @{NSForegroundColorAttributeName:RSD_ColorHex(@"0089FD")};
         appearance.stackedLayoutAppearance.normal.titleTextAttributes = @{NSForegroundColorAttributeName:RSD_ColorHex(@"454444")};
         [UITabBar appearance].standardAppearance = appearance;
@@ -22,4 +28,7 @@
            [[UITabBarItem appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName:RSD_ColorHex(@"454444")} forState:UIControlStateNormal];
            [[UITabBarItem appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName:RSD_ColorHex(@"0089FD")} forState:UIControlStateSelected];
         }
+        [UITabBar appearance].shadowImage = [UIImage rsd_imageWithColor:RSD_Color(240, 240, 240)size:CGSizeMake(1, 0.5)];
+        [UITabBar appearance].backgroundImage = [UIImage rsd_imageWithColor:RSD_Color(255, 255, 255)size:CGSizeMake(1, 0.5)];
+        [UITabBar appearance].translucent = NO;
     }
